628fbacb07f741de1293a5e8e7d260f558da9cfb
Matt Britt <matthewlbritt@gmail.com>
2018-04-21 22:15:45 -0500

    cleaned up code for main.js

--- main.js
+++ main.js
@@ -2,28 +2,31 @@
         main.js is the javscript that is served to the index.html main page of the app.
 */
 
+// server root url
 var url = "http://localhost:5000"
 
-var zipForm = document.getElementById("zipForm");
-console.log(zipForm)
-zipForm.addEventListener("submit", (event)=>{
+// add event listeners:
+
+// eventListener for zipcode form submit
+document.getElementById("zipForm").addEventListener("submit", (event)=>{
+    // prevent form submit event from "firing" so we stay on page, code will handle
     event.preventDefault();
 
-    console.log(event.target.zip.value);
-
+    // data to send to server
     var payload = {
         zip: event.target.zip.value
     }
-    console.log(payload)
 
+    // fetch POST request -- send payload and receive data in callback/promise
     fetch(url + "/receiver", {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(payload)
+
     }).then((data)=>{
+    // callback/promise to receive data from server and update map coords
         return data.json()})
         .then((data)=>{
-            console.log(data)
             myMap({
                 lat: data[0].lat,
                 lng: data[0].long

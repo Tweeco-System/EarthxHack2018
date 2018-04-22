3d787b02a40edbea026cc1a2503bfa6c61c32777
Matt Britt <matthewlbritt@gmail.com>
2018-04-22 01:44:55 -0500

    adding getTest.js - small node script to test front-end post

---
+++ Testing/getTest.js
@@ -0,0 +1,28 @@
+var express = require('express')
+var app = express()
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+
+app.post('/receiver', (req,res)=>{
+    var zipcode = req.body.zip
+    var category = req.body.hashtag
+    console.log(req.body)
+    console.log("zip: " + zipcode + ";  category: " + category)
+
+    dummy = [{
+        tweet: "People are all trashy or they are not",
+        lat: 37.2343,
+        long: -115.8067
+    }]
+
+    res.send(JSON.stringify(dummy))
+})
+
+
+var port = process.argv[2] || 6000
+
+app.listen(port);
+console.log("Server on port " + port)

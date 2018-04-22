1a3abd817333a51504cf0db5a2db228ac70004e7
vikramlance <vikram.lance@gmail.com>
2018-04-22 00:00:38 -0500

    style changes

---
+++ static/main.js
@@ -0,0 +1,36 @@
+/*
+        main.js is the javscript that is served to the index.html main page of the app.
+*/
+
+// server root url
+var url = "http://localhost:5000"
+
+// add event listeners:
+
+// eventListener for zipcode form submit
+document.getElementById("zipForm").addEventListener("submit", (event)=>{
+    // prevent form submit event from "firing" so we stay on page, code will handle
+    event.preventDefault();
+
+    // data to send to server
+    var payload = {
+        zip: event.target.zip.value,
+        hashtag: event.target.hashtag.value
+    }
+
+    // fetch POST request -- send payload and receive data in callback/promise
+    fetch(url + "/receiver", {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(payload)
+
+    }).then((data)=>{
+    // callback/promise to receive data from server and update map coords
+        return data.json()})
+        .then((data)=>{
+            myMap({
+                lat: data[0].lat,
+                lng: data[0].long
+            })
+        })})
+    
